9102075ea8c9e0c3d18929d20cfc6d569c355e28
Your Reimu <106019215+AstolfoOwO@users.noreply.github.com>
2022-08-31 13:48:46 +0700

    upd
    background banner

--- Touhou.land site/src/js/app.js
+++ Touhou.land site/src/js/app.js
@@ -22,34 +22,3 @@ function NavBar(){
         x.className="navbar";
     }
 }
-
-function scrollNav(){
-    $('.docs_list, .docs_list_to').click(function(){
-        $(".docs_active").removeClass("docs_active");
-        $(this).addClass("docs_active");
-        $('html, body').stop().animate({scrollTop:$($(this).attr('href')).offset().top-1},300);
-        return true;
-    }
-    );
-}
-scrollNav();
-function checkMeNSFW(selected){
-    if(selected)document.getElementById("violations_content").style.display="none";
-    else document.getElementById("violations_content").style.display="flex";
-}
-
-function autosize(){
-    var text=$('textarea');
-    text.each(function(){
-        $(this).attr('rows',1);
-        resize($(this));
-    });
-    text.on('input', function(){
-        resize($(this));
-    });
-    function resize($text){
-        $text.css('height','auto');
-        $text.css('height',$text[0].scrollHeight+'px');
-    }
-}
-autosize();
